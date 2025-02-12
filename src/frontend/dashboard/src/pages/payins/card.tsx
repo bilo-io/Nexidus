c2dd5ba5fe9c6606ae15b84e6ce90979f3147465
Bilo Lwabona <bilo.lwabona@gmail.com>
2025-02-12 23:23:13 +0200

    chore: changed to package-lock.json and fixed more build issues

--- src/frontend/dashboard/src/pages/payins/card.tsx
+++ src/frontend/dashboard/src/pages/payins/card.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { View } from '../../components/Core';
 import AppTopBar from '../../components/App/TopBar';
 import { useTranslation } from 'react-i18next';
-// import Charts from '../misc/charts';
-// import BarChart from '../../components/Core/Charts/react-chartjs-2/BarChart';
 
 type PayinsCardProps = object
 
--- src/frontend/dashboard/src/pages/recon/unallocated-funds.tsx
+++ src/frontend/dashboard/src/pages/recon/unallocated-funds.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Text, View } from '../../components/Core';
+import { View } from '../../components/Core';
 import AppTopBar from '../../components/App/TopBar';
-// import Charts from '../misc/charts';
-// import BarChart from '../../components/Core/Charts/react-chartjs-2/BarChart';
 
 type UnallocatedFundsProps = object
 
